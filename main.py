c56b767cf5d291f1370f6325fa91f93d28c8c509
ianpaulleong <58120012+ianpaulleong@users.noreply.github.com>
2019-12-28 15:03:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,52 +9,92 @@ from torchvision import transforms
 from torch.optim import lr_scheduler
 from utilityFunctions import loadJpgFile
 from utilityFunctions import loadTxtFile
+from utilityFunctions import flipLRTxtData
+from utilityFunctions import flipUDTxtData
 from utilityFunctions import train_model
 from utilityFunctions import compareAValImg
 
 # These are the tunable hyperparameters
 batchSize = 4
-learning_rate = .0000001
-numTrainingIters = 1
+#learning_rate = .0000001
+learning_rate = .001
+numTrainingIters = 12
 reloadData = 1
-reloadNetwork = 0
-step_size = 7
+recreateNetwork = 0
+loadNetworkFromFile = 1
+step_size = 3
 gamma = 0.1
-det_threshold = 0.45
+det_thresholdX = 0.45
+det_thresholdO = 0.38
 
 # SECTION 2: IMPORT TRAINING DATA
+basic_transform = transforms.Compose([
+    transforms.Resize(244),
+    transforms.ToTensor()
+])
+color_transform = transforms.Compose([
+    transforms.Resize(244),
+    transforms.ColorJitter(),
+    transforms.ToTensor()
+])
 
 # If it's loaded, don't reload the data. 
+# This determines what preprocessing will happen to the images. It might belong in the hyperparameters section
 if reloadData:
     # Create lists of all .jpg and .txt in the training data folder
     trainDir = 'Images/Train/'
     txtFileList = glob.glob(trainDir + '*.txt')
     jpgFileList = glob.glob(trainDir + '*.jpg')
     
-    
     # Prepare the lists that will contain the data
     jpgList = []
     txtList = []
-    
-    # This determines what preprocessing will happen to the images. It might belong in the hyperparameters section
-    data_transforms = transforms.Compose([
-        transforms.Resize(244),
-        #transforms.CenterCrop(224),
-        transforms.ToTensor()
-    ])
     
     # Load the data into the batch lists. 
     numFiles = len(txtFileList)
     for ii in range(numFiles):
         print('File number:',ii,'\n')
-        batchTxtData = loadTxtFile(txtFileList[ii])
-        batchJpgData = loadJpgFile(data_transforms,jpgFileList[ii])
-        txtList.append(batchTxtData)
-        jpgList.append(batchJpgData)
+        #Load data from files
+        curTxtData = loadTxtFile(txtFileList[ii])
+        curJpgData = loadJpgFile(basic_transform,jpgFileList[ii])
+        txtList.append(curTxtData)
+        jpgList.append(curJpgData)
+        
+        #Augment through flips!
+        curTxtData = flipLRTxtData(curTxtData)
+        curJpgData = curJpgData.flip([3])
+        txtList.append(curTxtData)
+        jpgList.append(curJpgData)
+        curTxtData = flipUDTxtData(curTxtData)
+        curJpgData = curJpgData.flip([2])
+        txtList.append(curTxtData)
+        jpgList.append(curJpgData)
+        curTxtData = flipLRTxtData(curTxtData)
+        curJpgData = curJpgData.flip([3])
+        txtList.append(curTxtData)
+        jpgList.append(curJpgData)
+        
+        #Augment through color jitter AND flips!
+        curTxtData = loadTxtFile(txtFileList[ii])
+        curJpgData = loadJpgFile(color_transform,jpgFileList[ii])
+        txtList.append(curTxtData)
+        jpgList.append(curJpgData)
+        curTxtData = flipLRTxtData(curTxtData)
+        curJpgData = curJpgData.flip([3])
+        txtList.append(curTxtData)
+        jpgList.append(curJpgData)
+        curTxtData = flipUDTxtData(curTxtData)
+        curJpgData = curJpgData.flip([2])
+        txtList.append(curTxtData)
+        jpgList.append(curJpgData)
+        curTxtData = flipLRTxtData(curTxtData)
+        curJpgData = curJpgData.flip([3])
+        txtList.append(curTxtData)
+        jpgList.append(curJpgData)
 
 # SECTION 3: LOAD PRETRAINED NETWORK, MODIFY, TRAIN
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-if reloadNetwork:
+if recreateNetwork:
     myModel = torchvision.models.resnet18(pretrained=True)
     num_ftrs = myModel.fc.in_features
     myModel.fc = nn.Linear(num_ftrs, 18)
@@ -62,6 +102,10 @@ if reloadNetwork:
 #    num_ftrs = myModel.classifier[6].in_features
 #    myModel.classifier[6] = nn.Linear(num_ftrs, 18)
     myModel = myModel.to(device)
+elif loadNetworkFromFile:
+    with open('retrainedModel.pickle', 'rb') as handle:
+        myModel = pickle.load(handle)
+
 myLossFunction = torch.nn.SmoothL1Loss()
 #myLossFunction = torch.nn.MSELoss()
 myOptimizer = torch.optim.Adam(myModel.parameters(), lr=learning_rate)
@@ -74,13 +118,13 @@ model_ft = train_model(device, batchSize, jpgList, txtList,'train', myModel, myL
 # SECTION 4: VALIDATION TIME!
 valJpgList = glob.glob('Images/Val/*.jpg')
 valTxtList = glob.glob('Images/Val/*.txt')
-compareAValImg(myModel,data_transforms,device,valTxtList,valJpgList,3, det_threshold)
-compareAValImg(myModel,data_transforms,device,valTxtList,valJpgList,4, det_threshold)
-compareAValImg(myModel,data_transforms,device,valTxtList,valJpgList,6)
-compareAValImg(myModel,data_transforms,device,valTxtList,valJpgList,10)
-compareAValImg(myModel,data_transforms,device,valTxtList,valJpgList,13)
-compareAValImg(myModel,data_transforms,device,valTxtList,valJpgList,15, det_threshold)
-compareAValImg(myModel,data_transforms,device,valTxtList,valJpgList,16)
+compareAValImg(myModel,basic_transform,device,valTxtList,valJpgList,3,det_thresholdX,det_thresholdO)
+compareAValImg(myModel,basic_transform,device,valTxtList,valJpgList,4,det_thresholdX,det_thresholdO)
+compareAValImg(myModel,basic_transform,device,valTxtList,valJpgList,6,det_thresholdX,det_thresholdO)
+compareAValImg(myModel,basic_transform,device,valTxtList,valJpgList,10,det_thresholdX,det_thresholdO)
+compareAValImg(myModel,basic_transform,device,valTxtList,valJpgList,13,det_thresholdX,det_thresholdO)
+compareAValImg(myModel,basic_transform,device,valTxtList,valJpgList,15,det_thresholdX,det_thresholdO)
+compareAValImg(myModel,basic_transform,device,valTxtList,valJpgList,16,det_thresholdX,det_thresholdO)
 
 # SECTION 5: Save the trained model!
 with open('retrainedModel.pickle', 'wb') as handle:
